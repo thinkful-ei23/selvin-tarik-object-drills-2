18b0aacc7853c8d24ce9253a8ec3aaa4a2ca3e15
Selvin Norman <selvin715@gmail.com>
2018-07-18 13:23:15 -0700

    added iteration over obj properties

--- object-drills-2.js
+++ object-drills-2.js
@@ -10,3 +10,13 @@ const loaf = {
 
 console.log(`flour: ${loaf.flour}; water: ${loaf.water}`);
 console.log(loaf.hydration());
+const myObj = {
+  foo: 'bird',
+  bar: 'fish',
+  fum: 'cat',
+  quux: 'car',
+  spam: 'ham'
+}
+for (let key in myObj) {
+  console.log(`key: ${key} | value: ${myObj[key]}`);
+}
