1b87533924b677053768c515539bb595aa6de210
Tarik Desire <tprdesire@gmail.com>
2018-07-18 16:46:22 -0400

    Added Arrays of Objects Exercise

--- object-drills-2.js
+++ object-drills-2.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//1. Object Initializers
 const loaf = {
   flour: 300,
   water: 210,
@@ -8,6 +9,9 @@ const loaf = {
   }
 };
 
+
+
+//2. Iterating Over An Object's Properties
 console.log(`flour: ${loaf.flour}; water: ${loaf.water}`);
 console.log(loaf.hydration());
 const myObj = {
@@ -20,7 +24,32 @@ const myObj = {
 for (let key in myObj) {
   console.log(`key: ${key} | value: ${myObj[key]}`);
 }
+
+
+
+//3. Arrays in Objects
 const mealObj = {
   meals : ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
 }
 console.log(`Hobbit's 4th meal: ${mealObj.meals[3]}`);
+
+
+
+//4. Arrays of Objects
+let charList = [
+  {
+    name: 'Frodo',
+    jobTitle: 'Hobbit'
+  },
+  {
+    name: 'Legolas',
+    jobTitle: 'Elf'
+  },
+  {
+    name: 'Gimli',
+    jobTitle: 'Dwarf'
+  }];
+
+charList.forEach(char => {
+  console.log(`Name: ${char['name']}; Job Title: ${char['jobTitle']}`);
+});
